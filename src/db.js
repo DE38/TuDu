36f4ade6fef2ca3e38294a11ca1d246ad08a6932
Yadusable <eiklautdaten@icloud.com>
2022-11-11 09:49:51 +0100

    fixed postgresql syntax

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
     user: "postgres",
     password: "postgres",
-    host: "localhost",
+    host: "192.168.0.100",
     port: 5432,
     database: "postgres"
 });
