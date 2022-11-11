e3f598d2b94bceb77f4dda44984a84a4e809b9c3
Yadusable <eiklautdaten@icloud.com>
2022-11-11 09:30:16 +0100

    typo...

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
     user: "postgres",
     password: "postgres",
-    host: "192.169.0.100",
+    host: "localhost",
     port: 5432,
     database: "postgres"
 });
