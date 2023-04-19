d221990e15d906dc55efa260eaca61f76a851cc6
Yadusable <eiklautdaten@icloud.com>
2023-04-19 10:23:09 +0200

    check for invalid params

--- src/auth.js
+++ src/auth.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require("body-parser");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const pool = require("./db.js")
 
@@ -13,15 +14,16 @@ app.use(bodyParser.json());
 module.exports = app.post('/v1/register', async (req, res) => {
     try {
         let {email, passwd} = req.body;
+        if (email === undefined || passwd === undefined){
+            res.status(400);
+            res.send({text: `email or password is missing`});
+        }
         try {
             await pool.query('INSERT INTO users (email, pw_hash) VALUES ($1, $2)', [email, passwd]);
         } catch {
             res.status(400);
             res.send({text: `this email has already been registered!`});
         }
-        const idResponse = await pool.query('SELECT user_id from users WHERE email = $1', [email]); //TODO remove
-        const id = idResponse.rows[0].user_id;
-
         res.status(201);
         res.send({text: `You have been registered!`});
     } catch (err) {
@@ -35,9 +37,7 @@ module.exports = app.post('/v1/login', async (req, res) => {
     try {
         const {email, passwd} = req.body;
         const pw_hash_from_db = (await pool.query('SELECT pw_hash FROM users WHERE email = $1', [email])).rows[0].pw_hash;
-        //        console.log("body: " + passwd);
-        //        console.log("DB: " + pw_hash_from_db);
-        if(passwd == pw_hash_from_db){
+        if(bcrypt.compare(passwd, pw_hash_from_db)){
             await pool.query('UPDATE users SET auth_token = $1 WHERE email = $2', ['auth_test_token', email]);
             await pool.query('UPDATE users SET refresh_token = $1 WHERE email = $2', ['refresh_test_token', email]);
             res.status(200);
