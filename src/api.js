a4ff4854a535217f2af9e8d70aa4643ed4af2cef
DE38 <niklas@gt-trenz.de>
2023-04-26 11:17:03 +0200

    move getUser to AUTH and fix middleware

--- src/api.js
+++ src/api.js
@@ -12,10 +12,9 @@ app.use(cookieParser());
 // MIDDLEWARES
 app.use(async (req, res, next) => {
     try {
-        const {email} = req.body;
+        const email = req.headers.email;
         const auth_token_req = req.cookies['auth_token'];
         const auth_token_db = (await pool.query('SELECT auth_token FROM users WHERE email = $1', [email])).rows[0].auth_token;
-//        console.log(auth_token_db + ";" + auth_token_req)
         if(auth_token_db != auth_token_req) {
             console.error('wrong login credentials')
             res.status(400).send({text: 'Your access token is invalid'});
@@ -28,20 +27,6 @@ app.use(async (req, res, next) => {
 });
 
 
-//USER
-
-
-module.exports = app.get('/v1/getUser', async (req, res) => {
-    // const auth_token_req = req.cookies['auth_token'];
-    // try {
-    //     const email = (await pool.query('SELECT email FROM users WHERE auth_token = $1', [auth_token_req])).rows[0].auth_token;
-    //     res.status(200).send({ email: `${email}`});
-    // } catch (err) {
-    //     console.error("HERE", err.message);
-    //     res.status(500).send();
-    // }
-    res.status(200).send({ email: 'Niklas' });
-})
 
 //Tasks
 module.exports = app.get('/v1/tasks/', async (req, res) => { //TODO BUG
@@ -134,7 +119,7 @@ module.exports = app.delete('/v1/tasks/:id', async (req, res) => {
 //Lists/ reoccuring
 module.exports = app.get('/v1/list/', async (req, res) => {
     try {
-        const {email} = req.body;
+        const email = req.headers.email;
         const idResponse = await pool.query('SELECT user_id from users WHERE email = $1', [email]);
         const userId = idResponse.rows[0].user_id;
         const queryResponse = await pool.query('SELECT * from reoccurring WHERE user_id = $1', [userId]);
@@ -147,7 +132,7 @@ module.exports = app.get('/v1/list/', async (req, res) => {
 
 module.exports = app.get('/v1/list/:id', async (req, res) => {
     try {
-        const {email} = req.body;
+        const email = req.headers.email;
         const idResponse = await pool.query('SELECT user_id from users WHERE email = $1', [email]);
         const userId = idResponse.rows[0].user_id;
         const reqId = req.params.id;
