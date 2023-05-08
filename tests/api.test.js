3e367053ddd9295400e8f792e9edd9d3dd7fda56
DE38 <niklas@gt-trenz.de>
2023-05-08 13:01:43 +0200

    add get list test (wip)

--- tests/api.test.js
+++ tests/api.test.js
@@ -16,9 +16,9 @@ describe('GET /v1/tasks', function() {
             }
         });
     });
-    
-    
-    
+
+
+
     it('make the right SQL queries', async () => {
       let fakeMethod = sinon.fake((param1, param2) => {
           if (param1 === 'SELECT user_id from users WHERE email = $1' && JSON.stringify(param2) === JSON.stringify(['test@gmail.com'])){
@@ -41,3 +41,35 @@ describe('GET /v1/tasks', function() {
   });
 });
 
+describe('GET /v1/list', function () {
+    beforeEach(() => {
+        const originalMiddleware = api._router.stack.find(layer => layer.handle === JWTmiddleware).handle;
+        const middlewareMock = jest.fn((req, res, next) => {
+            next();
+        });
+        api._router.stack.forEach(layer => {
+            if (layer.handle === originalMiddleware) {
+                layer.handle = middlewareMock;
+            }
+        });
+    });
+
+    it('request all lists', async () => {
+        let fakeGetLists = sinon.fake((param1, param2) => {
+            if (param1 === 'SELECT user_id from users WHERE email = $1' && JSON.stringify(param2) === JSON.stringify(['test@gmail.com'])){
+                return(
+                    {"rows" :  [{"user_id": 43}] }
+                );
+            } else if (param1 === 'SELECT * from list WHERE user_id = $1' && JSON.stringify(param2) === JSON.stringify([ 43 ])){
+                return(
+                    {"rows" :  ["touch grass"] }
+                );
+            }
+        });
+        sinon.replace(pool, 'query', fakeGetLists);
+
+        const response = await request(api).get('/v1/list').send({email: "test@gmail.com"});
+        expect(response.status).toBe(200);
+        expect(fakeGetLists.called).toBe(true);
+    })
+})
